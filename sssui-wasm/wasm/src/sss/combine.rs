65975d8a824782e9da8a4e4c496ddb98a79cb232
Calvin Lee <jinwoo0451@naver.com>
2025-09-21 02:47:47 +0900

    add TODO

--- sssui-wasm/wasm/src/sss/combine.rs
+++ sssui-wasm/wasm/src/sss/combine.rs
@@ -1,16 +1,21 @@
 use gloo_utils::format::JsValueSerdeExt;
 use k256::Secp256k1;
+use p256::NistP256;
 use sssui_rs::point::Point256;
 use sssui_rs::sss::combine;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn combine_ec(points: JsValue, t: u32) -> Result<JsValue, JsValue> {
-    let points: Vec<Point256> = points
-        .into_serde()
-        .map_err(|err| JsValue::from_str(&err.to_string()))?;
+pub fn combine_ec(points: JsValue, t: u32, curve: String) -> Result<JsValue, JsValue> {
+    // TODO @Hyeong-soo
+    // curve = "secp256k1" | "secp256r1" 둘 중 하나가 되어야 하고 나머지는 그냥 에러 던지기
 
-    let out = combine::<Secp256k1>(points, t).map_err(|err| JsValue::from_str(&err.to_string()))?;
+    unimplemented!()
+    // let points: Vec<Point256> = points
+    //     .into_serde()
+    //     .map_err(|err| JsValue::from_str(&err.to_string()))?;
 
-    JsValue::from_serde(&out).map_err(|err| JsValue::from_str(&err.to_string()))
+    // let out = combine::<Secp256k1>(points, t).map_err(|err| JsValue::from_str(&err.to_string()))?;
+
+    // JsValue::from_serde(&out).map_err(|err| JsValue::from_str(&err.to_string()))
 }
--- sssui-wasm/wasm/src/sss/split.rs
+++ sssui-wasm/wasm/src/sss/split.rs
@@ -1,19 +1,30 @@
 use gloo_utils::format::JsValueSerdeExt;
 use k256::Secp256k1;
+use p256::NistP256;
 use sssui_rs::sss::split;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn split_ec(secret: JsValue, ks_node_hashes: JsValue, t: u32) -> Result<JsValue, JsValue> {
-    let secret: [u8; 32] = secret
-        .into_serde()
-        .map_err(|err| JsValue::from_str(&err.to_string()))?;
-    let ks_node_hashes: Vec<[u8; 32]> = ks_node_hashes
-        .into_serde()
-        .map_err(|err| JsValue::from_str(&err.to_string()))?;
+pub fn split_ec(
+    secret: JsValue,
+    // ks_node_hashes: JsValue, -> point_xs: JsValue
+    point_xs: JsValue,
+    t: u32,
+    curve: String,
+) -> Result<JsValue, JsValue> {
+    // TODO @Hyeong-soo
+    // curve = "secp256k1" | "secp256r1" 둘 중 하나가 되어야 하고 나머지는 그냥 에러 던지기
 
-    let out = split::<Secp256k1>(secret, ks_node_hashes, t)
-        .map_err(|err| JsValue::from_str(&err.to_string()))?;
+    unimplemented!()
+    // let secret: [u8; 32] = secret
+    //     .into_serde()
+    //     .map_err(|err| JsValue::from_str(&err.to_string()))?;
+    // let ks_node_hashes: Vec<[u8; 32]> = ks_node_hashes
+    //     .into_serde()
+    //     .map_err(|err| JsValue::from_str(&err.to_string()))?;
 
-    JsValue::from_serde(&out).map_err(|err| JsValue::from_str(&err.to_string()))
+    // let out = split::<Secp256k1>(secret, ks_node_hashes, t)
+    //     .map_err(|err| JsValue::from_str(&err.to_string()))?;
+
+    // JsValue::from_serde(&out).map_err(|err| JsValue::from_str(&err.to_string()))
 }
